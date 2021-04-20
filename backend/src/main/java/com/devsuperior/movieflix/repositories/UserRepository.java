9cee75f4b30dbc969aed8156bb0abb7e3cef56e9
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-04-20 19:27:22 +0100

    Security and login done

--- backend/src/main/java/com/devsuperior/movieflix/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/movieflix/repositories/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.movieflix.entities.Role;
 import com.devsuperior.movieflix.entities.User;
 
-public interface UserRepositories extends JpaRepository<User, Long>{
-
+public interface UserRepository extends JpaRepository<User, Long>{
+	User findByEmail(String email);
 }
